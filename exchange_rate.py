a7b7fc16985fca678d2fccf09f03f91b8aa75fab
ropraname <45720319+ropraname@users.noreply.github.com>
2020-04-06 21:16:25 +0300

    Update exchange_rate.py

--- exchange_rate.py
+++ exchange_rate.py
@@ -4,17 +4,30 @@ from bs4 import BeautifulSoup
 def get_best_exchange_rate(currency_code, num_branches):
 	url = 'https://www.banki.ru/products/currency/cash/{}/sankt-peterburg/#sort=sale&order=asc'.format(currency_code)
 	content = re.get(url)
-	content = content.text
-	soup = BeautifulSoup(content, 'html.parser')
-	#prettify()
-	all_bank_values = soup.find_all("div", class_='exchange-calculator-rates table-flex__row-group ')
+	soup = BeautifulSoup(content.text, "html.parser")
+
+	all_values = soup.find_all('div', class_="table-flex__row item calculator-hover-icon__container")
 	data = []
-	for banks in all_bank_values:
-		name = str(banks.find("a", class_='font-size-default color-gray-gray').text)
-		name_ard = str(banks.find("a", class_='font-bold').text)
-		value = banks.find("div", class_='table-flex__cell table-flex__rate font-size-large color-pumpkin-orange  text-nowrap')
-		value = str(value['data-currencies-rate-sell'])
-		container = (name, name_ard, value)
+	
+	for value in range(len(all_values)):
+		name = str(all_values[value].find("a", class_='font-size-default color-gray-gray').text)
+		name_ard = str(all_values[value].find("a", class_='font-bold').text)
+		if value == 0:
+
+			price = str(all_values[value].find_all('div')[6]["data-currencies-rate-sell"])
+
+		else:
+			divs = all_values[value].find_all('div')
+			for tags in divs:
+				try:
+					price = str(tags["data-currencies-rate-sell"])
+					if len(price) > 3:
+						price = price[:5]
+				except:
+					continue
+		container = (name, name_ard, price)
 		data.append(container)
 		
-	print(data[:num_branches])
+	return data[:num_branches]
+
+

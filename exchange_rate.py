8fe5a215050bcf0d13a53c9e08b20c8ae7f23080
ropraname <45720319+ropraname@users.noreply.github.com>
2020-04-06 21:26:30 +0300

    Update exchange_rate.py

--- exchange_rate.py
+++ exchange_rate.py
@@ -2,27 +2,29 @@ import requests as re
 from bs4 import BeautifulSoup
 
 def get_best_exchange_rate(currency_code, num_branches):
-	url = 'https://www.banki.ru/products/currency/cash/{}/sankt-peterburg/#sort=sale&order=asc'.format(currency_code)
+	url = 'https://www.banki.ru/products/currency/cash/{}/sankt-peterburg/#sort=sale&order=asc'.format(currency_code.lower())
 	content = re.get(url)
 	soup = BeautifulSoup(content.text, "html.parser")
 
 	all_values = soup.find_all('div', class_="table-flex__row item calculator-hover-icon__container")
 	data = []
-	
+	if len(all_values) < num_branches:
+		raise IndexError("Num of brunces is too big")
+
 	for value in range(len(all_values)):
 		name = str(all_values[value].find("a", class_='font-size-default color-gray-gray').text)
 		name_ard = str(all_values[value].find("a", class_='font-bold').text)
 		if value == 0:
 
-			price = str(all_values[value].find_all('div')[6]["data-currencies-rate-sell"])
+			price = float(all_values[value].find_all('div')[6]["data-currencies-rate-sell"])
 
 		else:
 			divs = all_values[value].find_all('div')
 			for tags in divs:
 				try:
-					price = str(tags["data-currencies-rate-sell"])
+					price = float(tags["data-currencies-rate-sell"])
 					if len(price) > 3:
-						price = price[:5]
+						price = float(str(price[:5]))
 				except:
 					continue
 		container = (name, name_ard, price)
@@ -30,4 +32,5 @@ def get_best_exchange_rate(currency_code, num_branches):
 		
 	return data[:num_branches]
 
+print(get_best_exchange_rate("usd", 3))
 

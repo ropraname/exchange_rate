7d275919f03b77a900ca554f363138d3ea908b1c
ropraname <45720319+ropraname@users.noreply.github.com>
2020-04-07 21:26:57 +0300

    Update test_public.py

--- test_public.py
+++ test_public.py
@@ -1,32 +1,56 @@
 import pytest
-
 from exchange_rate import get_best_exchange_rate
+from random import randint
 
-def test_small_big_brunhes():
+
+currencies_first = ["usd", "eur", "gbp"]
+max_num_branches_first = [3, 5, 2]
+
+currencies_second = ["abc", "f", "gg"]
+max_num_branches_second = [3, 1, 2]
+max_num_branches_last = [-3.15, -1, -2.5]
+
+test_params_first = list(zip(currencies_first, max_num_branches_first))
+test_params_second = list(zip(currencies_second, max_num_branches_second))
+test_params_last = list(zip(currencies_second, max_num_branches_last))
+
+@pytest.mark.parametrize("currency, max_num_branches", test_params_first)
+def test_correct_work(currency, max_num_branches):
+    num_branches = randint(1, max_num_branches)
+    assert len(get_best_exchange_rate(currency, num_branches)) == num_branches
+
+@pytest.mark.parametrize("currency, num_branches", test_params_first)
+def test_peak_num_branches(currency, num_branches):
     with pytest.raises(IndexError) as excinfo:
-        excinfo = get_best_exchange_rate("RUB", 0)
-    assert str(excinfo.value) == 'Num of brunces is too small'
-    
-    with pytest.raises(IndexError) as excinfo:
-        excinfo = get_best_exchange_rate("RUB", len(all_values))
+        num_branches = num_branches + 10000
+        excinfo = get_best_exchange_rate(currency, num_branches)
     assert str(excinfo.value) == 'Num of brunces is too big'
 
-def test_types_of_elems():
-    excinfo = get_best_exchange_rate("EUR", 3)
-    assert type(excinfo[0]) == tuple
-    assert type(excinfo[0][0]) == str
-    assert type(excinfo[0][1]) == str
-    assert type(excinfo[0][2]) == float
+@pytest.mark.parametrize("currency, num_branches", test_params_second)
+def test_correct_currency(currency, num_branches):
+    with pytest.raises(ValueError) as excinfo:
+        excinfo = get_best_exchange_rate(currency, num_branches)
+    assert str(excinfo.value) == 'There is now such currency'
 
-def test_len_num_of_elems():
-    num_branches = 7
-    currency_code = "EUR"
-    excinfo = get_best_exchange_rate(currency_code, num_branches)
-    assert len(excinfo) == num_branches
+@pytest.mark.parametrize("currency, num_branches", test_params_last)
+def test_type_num_branches(currency, num_branches):
+    with pytest.raises(ValueError) as excinfo:
+        excinfo = get_best_exchange_rate(currency, num_branches)
+    assert str(excinfo.value) == 'num_branches is uncorrect type'
 
-def test_sort():
-    num_branches = 10
-    currency_code = "EUR"
-    excinfo = get_best_exchange_rate(currency_code, num_branches)
-    sorted_excinfo = sorted(excinfo, key=lambda tup: tup[2])
-    assert excinfo == sorted_excinfo
+@pytest.mark.parametrize("currency, num_branches", test_params_first)
+def test_sorted(currency, num_branches):
+    excinfo = get_best_exchange_rate(currency, num_branches)
+    sorted_in_f = []
+    for elem in excinfo:
+        sorted_in_f.append(elem[2])
+    assert sorted_in_f == sorted(sorted_in_f)    
+
+@pytest.mark.parametrize("currency, num_branches", test_params_first)
+def test_type_of_elems_in_turple(currency, num_branches):
+    excinfo = get_best_exchange_rate(currency, num_branches)
+    for elem in excinfo:
+        assert type(elem) == tuple
+        assert type(elem[0]) == str
+        assert type(elem[1]) == str
+        assert type(elem[2]) == float
